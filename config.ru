bbd3ebd0c381ef7e24f3981369b4a465740b001b
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-08-30 20:36:37 +0100

    Committing before more changes

--- config.ru
+++ config.ru
@@ -4,7 +4,11 @@ begin
   fi_check_migration
 
   use Rack::MethodOverride
+  use GenresController
+  use SongsController
+  use ArtistsController
   run ApplicationController
+
 rescue ActiveRecord::PendingMigrationError => err
   STDERR.puts err
   exit 1
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,10 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
+# madeline = Author.create(name: "Madeline L'Engle")
+# GoogleBooks::Adapter.new(madeline).fetch_books
+
+
+
+Song.create(name: "That One with the Guitar" )
+Genre.create(name: "New Age Garbage" )
+Artist.create(name: "Person with a Face" )
